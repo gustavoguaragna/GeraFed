ae05505a0d74de60f9e7f77b03482166caa50588
Guaragna <78802289+Guaragna@users.noreply.github.com>
2024-12-05 10:32:09 -0300

    acertando tamanho de transform para mnist

--- Simulation/task.py
+++ Simulation/task.py
@@ -49,7 +49,7 @@ def load_data(partition_id: int, num_partitions: int):
     # Divide data on each node: 80% train, 20% test
     partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
     pytorch_transforms = Compose(
-        [ToTensor(), Normalize((0.5, 0.5), (0.5, 0.5))]
+        [ToTensor(), Normalize((0.5,), (0.5,))]
     )
 
     def apply_transforms(batch):
